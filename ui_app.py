6a0b4a49ae38fda441d8e442bf07b4ea2f1921e0
Siddharth Mishra <mishra.kinshu@gmail.com>
2025-10-21 19:39:50 +0300

    offfi

--- ui_app.py
+++ ui_app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
